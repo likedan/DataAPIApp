cacff3cb42e3fb8bd9c03777aac69d6f23c6aeb2
Kedan Li <likedan5@icloud.com>
2016-07-24 23:10:40 -0700

    sign up data saved to database

--- WebApp/app/signup.py
+++ WebApp/app/signup.py
@@ -1,22 +1,37 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from flask_wtf import Form
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Email, Length
+from flask_pymongo import PyMongo
+
+mongo = PyMongo(app)
 
 class SignUpForm(Form):
-    email = StringField('email', validators=[DataRequired(), Email("invalid email address")])
+    email = StringField('email', validators=[DataRequired(), Email()])
     full_name = StringField('full_name', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired(), Length(min=8)])
+    password = PasswordField('password', validators=[DataRequired()])
 
 @app.route('/signup', methods=['GET'])
 
 def signup():
     form = SignUpForm()
-    if form.validate_on_submit():
-        print "!!!!"
-        # flash('Login requested for OpenID="%s", remember_me=%s' %
-        #       (form.openid.data, str(form.remember_me.data)))
-        return render_template("index.html")
-    print "?????"
     return render_template("signup.html", form=form)
+
+@app.route('/signupform', methods=['POST'])
+def signupform():
+    form = SignUpForm(request.form)
+    print form.validate()
+    error_list = []
+    print form.errors
+    if form.validate():
+        if mongo.db.users.find_one({'email': form.email.data}):
+            error_list.append("This email is associated with another account, please use a different one.")
+        else:
+            mongo.db.users.insert({"email": form.email.data, "full_name":form.full_name.data, "password": form.password.data})
+            return render_template("index.html")
+    else:
+        for key in form.errors.keys():
+            error_list.append(key+": "+form.errors[key][0])
+    #only displaying one error for now.                     
+    return render_template("signup.html", form=form, error=error_list[0])
