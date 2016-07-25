228dece8d75b1331be90ebb8c8165b3ad7a5a657
Kedan Li <likedan5@icloud.com>
2016-07-24 21:41:45 -0700

    implementing sign up

--- WebApp/app/__init__.py
+++ WebApp/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
+app.config.from_object('config')
 from app import index
 from app import signup
 
--- WebApp/app/signup.py
+++ WebApp/app/signup.py
@@ -1,8 +1,22 @@
 from flask import render_template
 from app import app
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email, Length
 
-@app.route('/signup', methods=['GET', 'POST'])
+class SignUpForm(Form):
+    email = StringField('email', validators=[DataRequired(), Email("invalid email address")])
+    full_name = StringField('full_name', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired(), Length(min=8)])
+
+@app.route('/signup', methods=['GET'])
 
 def signup():
-    print "event"
-    return render_template("signup.html")
+    form = SignUpForm()
+    if form.validate_on_submit():
+        print "!!!!"
+        # flash('Login requested for OpenID="%s", remember_me=%s' %
+        #       (form.openid.data, str(form.remember_me.data)))
+        return render_template("index.html")
+    print "?????"
+    return render_template("signup.html", form=form)
