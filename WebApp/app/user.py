adb1cdbcae04f23c8da4d77053562a1728a4c58f
Kedan Li <likedan5@icloud.com>
2016-07-27 21:43:13 -0700

    get user dynamically

--- WebApp/app/user.py
+++ WebApp/app/user.py
@@ -7,12 +7,8 @@ class User:
         user_info = mongo.db.users.find_one({'email': email})
         if user_info != None:
             print user_info
-            self.password_hash = user_info["password_hash"]
-            self.email = email
-            self.full_name = user_info["full_name"]
-            self.is_email_authenticated = user_info["is_email_authenticated"]
-            if not self.is_email_authenticated:
-                self.email_confirmation_token = user_info["email_confirmation_token"]
+            for key in user_info.keys():
+                setattr(self, key, user_info[key])
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
